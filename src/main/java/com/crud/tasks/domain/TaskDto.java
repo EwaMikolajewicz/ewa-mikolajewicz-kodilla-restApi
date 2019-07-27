d1b7c33d917ba2fa7c65c9fb43054efb6acc5d31
EwaMikolajewicz <mikolajewicz.e@gmail.com>
2019-07-27 16:13:33 +0200

    Zadanie 19.3

--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -2,12 +2,18 @@ package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Getter
 @AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class TaskDto {
     private Long id;
     private String title;
     private String content;
 
+    public TaskDto(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
---
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -0,0 +1,23 @@
+package com.crud.tasks.repository;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
+import org.springframework.data.repository.CrudRepository;
+
+import java.awt.font.OpenType;
+import java.util.List;
+import java.util.Optional;
+
+public interface TaskRepository extends CrudRepository<Task, Long> {
+    @Override
+    List<Task> findAll();
+
+    @Override
+    Task findOne(Long taskId);
+
+    @Override
+    Task save(Task task);
+
+    @Override
+    void delete(Long taskId);
+}
